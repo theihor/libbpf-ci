a5c1cba1b3e892f5e1ae1fff29571abd7ca3fa68
Ihor Solodrai <ihor.solodrai@pm.me>
2024-09-11 18:26:22 -0700

    Add simplified-kbuild workflow

--- .github/scripts/get-commit-metadata.sh
+++ .github/scripts/get-commit-metadata.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-branch="${GITHUB_BASE_REF}"
+# branch="${GITHUB_BASE_REF}"
 
-if [ "${GITHUB_EVENT_NAME}" = 'push' ]; then
-  branch="${GITHUB_REF_NAME}"
-fi
+# if [ "${GITHUB_EVENT_NAME}" = 'push' ]; then
+#   branch="${GITHUB_REF_NAME}"
+# fi
+
+set -eu
+
+branch=${1:-bpf-next_base}
 
 echo "branch=${branch}" >> "${GITHUB_OUTPUT}"
 
 upstream="${branch//_base/}"
-commit="$(
-  git rev-parse "origin/${upstream}" &> /dev/null \
-    || (
-      git fetch --quiet --prune --no-tags --depth=1 --no-recurse-submodules origin "+refs/heads/${upstream}:refs/remotes/origin/${upstream}" && \
-      git rev-parse "origin/${upstream}"
-    )
-)"
+
+commit=$(git rev-parse "origin/${upstream}" 2> /dev/null)
+if [ -z "${commit}" ]; then
+  echo "Could not rev-parse commit for origin/${upstream}, fetching..."
+  git fetch --quiet --prune --no-tags --depth=1 --no-recurse-submodules \
+      origin "+refs/heads/${upstream}:refs/remotes/origin/${upstream}"
+  commit=$(git rev-parse "origin/${upstream}")
+fi
+
 timestamp_utc="$(TZ=utc git show --format='%cd' --no-patch --date=iso-strict-local "${commit}")"
 
 echo "timestamp=${timestamp_utc}" >> "${GITHUB_OUTPUT}"
